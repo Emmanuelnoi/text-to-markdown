c4c95eca861d70ad1f6c1b0d4ad150a61868e808
Emmanuel Noi <Emmanuel.noi@student.fairfield.edu>
2025-04-04 19:44:28 -0700

    updated UX/UI

--- src/app/richtext/richtext.component.ts
+++ src/app/richtext/richtext.component.ts
@@ -1,63 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, Signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { LucideAngularModule, Heading, Bold, Italic, Strikethrough,Underline } from 'lucide-angular';
 
-import { Editor } from '@tiptap/core';
-import Placeholder from '@tiptap/extension-placeholder';
-import StarterKit from '@tiptap/starter-kit';
-import TiptapUnderline from '@tiptap/extension-underline'
-import TiptapHeading from '@tiptap/extension-heading';
 import { TiptapBubbleMenuDirective, TiptapEditorDirective } from 'ngx-tiptap';
+import { EditorService } from '../services/editor.service';
 
 @Component({
   selector: 'app-richtext',
-  imports: [ CommonModule, FormsModule, TiptapBubbleMenuDirective, TiptapEditorDirective, 
-    LucideAngularModule ],
+  imports: [ 
+    CommonModule, 
+    FormsModule, 
+    TiptapBubbleMenuDirective, 
+    TiptapEditorDirective, 
+    LucideAngularModule 
+  ],
   templateUrl: './richtext.component.html',
   styleUrl: './richtext.component.css'
 })
-export class RichtextComponent {
-  
+export class RichtextComponent implements OnInit, OnDestroy{
+
   readonly Heading = Heading;
   readonly Bold = Bold;
   readonly Italic = Italic;
   readonly Strikethrough = Strikethrough;
   readonly Underline = Underline;
 
-  editor! : Editor;
-  // value = `"This is a Test run. Remember: Power to you!"`;
+  private editorService = inject(EditorService); //inject editorService using 'inject'
+  content: Signal<string> = this.editorService.content // Bind signal directly
 
-  content = signal<string>('');
-  constructor(){
-    this.initializeEditor();
-  }
+  editor = this.editorService.getEditor()
 
-  private initializeEditor(){
-    this.editor = new Editor({
-      extensions: [
-        StarterKit,
-        Placeholder,
-        TiptapUnderline,
-        TiptapHeading.configure({
-          levels: [1],
-        })
-      ],
-      editorProps: {
-        attributes: {
-          class: 'mt-4 p-10 border-black hover:border-blue-700 focus:border-red-700 border-1 rounded-md outline-none',
-          spellCheck: 'false',
-        },
-      },
-      onUpdate: ({ editor }) => {
-        this.content.set(editor.getHTML()); // Update signal when content changes
-      }
-    });
-  }
+  ngOnInit() {}
 
-  // Destroy editor when component is removed
-  ngOnDestroy() {
-    this.editor.destroy();
+  ngOnDestroy(){
+    this.editorService.destroyEditor();
   }
 
 }
--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -1,65 +1,58 @@
-import { Component, Signal, signal, WritableSignal } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal, Signal } from '@angular/core';
 import { LucideAngularModule, Keyboard, ClipboardCopy, ArrowDownToLine, BadgeHelp,RefreshCcw } from 'lucide-angular';
 import { RichtextComponent } from "../richtext/richtext.component";
-import TurndownService from 'turndown'
+import { EditorService } from '../services/editor.service';
 
 @Component({
   selector: 'app-ui',
-  imports: [LucideAngularModule, RichtextComponent],
+  imports: [
+    LucideAngularModule, 
+    RichtextComponent
+  ],
   templateUrl: './ui.component.html',
   styleUrl: './ui.component.css'
 })
-export class UiComponent {
+export class UiComponent implements OnInit, OnDestroy{
   readonly Keyboard = Keyboard;
   readonly ClipboardCopy = ClipboardCopy;
   readonly ArrowDownToLine = ArrowDownToLine;
   readonly BadgeHelp = BadgeHelp;
   readonly RefreshCcw = RefreshCcw;
 
-  // Signal to store child data
-  editorContent: WritableSignal<string> = signal('');
-  markdownOutput: string = '';
-  turndownService = new TurndownService(); // Initialize Turndown
+  private editorService = inject(EditorService);
 
-  //run both functions
-  convertToMarkdown(contentSignal: WritableSignal<string>){
-    this.receiveContent(contentSignal);
-    this.convertMarkdown();
-  }
+  content: Signal<string> = this.editorService.content // Get editor content
+  markdownContent: Signal<string> =this.editorService.markdownContent; // GEt markdown content
 
-  // Function to get content from child
-  receiveContent(signalContent: WritableSignal<string>){
-    this.editorContent = signalContent;
-  }
-
-  // Convert Tiptap's HTML Content to Markdown
+  // Signal to control <pre> visibility
+  showMarkdown = signal(false);
+  
+ 
   convertMarkdown() {
-    this.markdownOutput = this.turndownService.turndown(this.editorContent());
+    this.editorService.convertToMarkdown(); // Convert to Markdown
+
+
+    // show <pre> only after clicking
+    this.showMarkdown.set(true);
   }
+  ngOnInit() {}
 
-
-  showKeyboardShortcuts(){
-
-  }
+  showKeyboardShortcuts(){}
 
   // Copy Markdown to Clipboard
   copyToClipboard(){
-    navigator.clipboard.writeText(this.markdownOutput).then(() => {
-      alert('Markdown copied to clipboard! ✅');
-    });
+    this.editorService.copyToClipboard();
   }
 
   // Download Markdown to Clipboard
   downloadMarkdown(){
-    const blob = new Blob([this.markdownOutput], { type: 'text/markdown' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'converted.md';
-    a.click();
-    URL.revokeObjectURL(url);
+    this.editorService.downloadMarkdown();
   }
 
   showHelp(){}
+
+  ngOnDestroy(): void {
+      this.editorService.destroyEditor();
+  }
 
 } 
