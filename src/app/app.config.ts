26936258d0096bce19ff4825fb372082382a8973
Emmanuel Noi <noigha@gmail.com>
2025-10-12 22:01:08 -0700

    fix: update lock file to sync dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@an
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { GlobalErrorHandler } from './services/error-handler.service';
 
@@ -10,7 +9,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideAnimations(),
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
   ],
